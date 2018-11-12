7775ef206126482f0bb3b504fcffbfd72f668802
Jaideep Singh Heer <jaideesinghheer@gmail.com>
2018-11-12 20:37:58 +0530

    I don't remember what i did but a lot has been done. - Created BT adapter and serial handler classes for serial communication wi the robot. - Created a new layout 'mainpage' and set main activity to always be landscape. - This new layout is planned to be the only layout.

--- app/src/main/java/jaideepheer/robo/roverone/robocontroller/MainActivity.java
+++ app/src/main/java/jaideepheer/robo/roverone/robocontroller/MainActivity.java
@@ -1,9 +1,17 @@
 package jaideepheer.robo.roverone.robocontroller;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import jaideepheer.robo.roverone.robocontroller.BluetoothClasses.BTAdapterHandler;
 
 public class MainActivity extends AppCompatActivity {
     public static Context mainContext;
@@ -11,21 +19,58 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.mainpage);
         mainContext = getApplicationContext();
         mainActivity = MainActivity.this;
-        init();
+        //init();
     }
     private void init()
     {
         // get ui elements
+        staticContext.latencyView = findViewById(R.id.latencyView);
         staticContext.debugTextView = findViewById(R.id.debugTextView);
+        staticContext.debugTextView.setMovementMethod(new ScrollingMovementMethod());
         staticContext.joystick = findViewById(R.id.mainJoyStick);
         (findViewById(R.id.select_bt)).setOnClickListener((view->{
-            BTDeviceHandler.selectBTDevice();}));
+            BTAdapterHandler.selectAndConnectBTDevice();}));
         // init.
-        BTDeviceHandler.init();
+        BTAdapterHandler.init();
         // set joystick update func.
         staticContext.joystick.setOnMoveListener(controllerState.getJoystickUpdater());
     }
+    private void initIntentFilter()
+    {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
+        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        this.registerReceiver(mReceiver, filter);
+    }
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+             //Device found
+            }
+            else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+             //Device is now connected
+                staticContext.debugPrint("Device connect intent.");
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+             //Done searching
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) {
+             //Device is about to disconnect
+                staticContext.debugPrint("Device disconnect requested intent.");
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+             //Device has disconnected
+                staticContext.debugPrint("Device disconnected intent.");
+            }
+        }
+    };
 }
--- app/src/main/java/jaideepheer/robo/roverone/robocontroller/staticContext.java
+++ app/src/main/java/jaideepheer/robo/roverone/robocontroller/staticContext.java
@@ -6,10 +6,11 @@ import io.github.controlwear.virtual.joystick.android.JoystickView;
 public class staticContext {
     public static JoystickView joystick;
     public static TextView debugTextView;
+    public static TextView latencyView;
 
     public static void debugPrint(Object o)
     {
-        System.out.println(o);
-        debugTextView.setText(debugTextView.getText()+"\n"+o);
+        System.out.println("Log: "+o);
+        debugTextView.append("\nLog: "+o);
     }
 }
